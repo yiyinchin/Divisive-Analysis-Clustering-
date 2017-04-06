044f55309c8c045f83f90a0c3c74408014ad04d7
yiyinchin <cyy92@outlook.com>
2017-04-06 13:28:24 +1200

    Update smallCal.scala

--- smallCal.scala
+++ smallCal.scala
@@ -1,7 +1,7 @@
 package org.apache.spark.mllib
 
 import org.apache.spark.internal.Logging
-import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, IndexedRow, MatrixEntry}
+import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, IndexedRow}
 import org.apache.spark.rdd.RDD
 
 object smallCal extends Logging {
@@ -12,10 +12,34 @@ object smallCal extends Logging {
     */
 
   def averageDissOO (
-                    p: RDD[MatrixEntry]
-                    ): Array[Double] ={
-    //Create a Coordinate Matrix from an RDD[MatrixEntry]
-    val x: CoordinateMatrix = new CoordinateMatrix(p)
+                    x: CoordinateMatrix
+                    ): Double ={
+    // Get its size
+    val m = x.numRows()
+    val n = x.numCols()
+
+    // Sum of the elements in the row of matrix
+    val sumRow = x.toIndexedRowMatrix.rows.map{
+      case IndexedRow(i, value) => value.toArray.sum
+    }
+
+    // Find the average of dissimilarity
+    val aveRow = sumRow.map{
+      case (value) => value / (m-1)
+    }
+
+    //TODO find the largest non negative average value
+    val p = aveRow.max
+    p
+  }
+
+  /**
+    * Average dissimilarity to Objects of Splinter Group
+    */
+
+  def averageDissSG(
+                   x: CoordinateMatrix
+                   ): RDD[Double] ={
 
     // Get its size
     val m = x.numRows()
@@ -23,35 +47,25 @@ object smallCal extends Logging {
 
     // Sum of the elements in the row of matrix
     val sumRow = x.toIndexedRowMatrix.rows.map{
-      case IndexedRow(i, value) => (value.toArray.sum)
+      case IndexedRow(i, value) => value.toArray.sum
     }
 
     // Find the average of dissimilarity
     val aveRow = sumRow.map{
-      case (value) => (value / (m-1))
+      case value => value / n
     }
-    
-    //TODO find the largest non negative average value
-    
-  }
-  
-    /**
-    * Average dissimilarity to Objects of Splinter Group
-    */
 
-  def averageDissSG(
-                   x: CoordinateMatrix
-                   ): Double ={
-    // Choose the splinter group row only
-    0.0
+    // Only select the average of dissimilarity of the Remaining objects, without the splinter group
+    //TODO
+    aveRow
   }
-  
+
   /**
     * Difference between average dissimilarity to Objects and average dissimilarity to Objects of Splinter Group
     *
     * @param x Average Dissimilarity to Remaining Objects
     * @param y Average Dissimilarity to Objects to Splinter Group
-    * @return the difference 
+    * @return the difference
     */
 
   def diff(
@@ -70,6 +84,5 @@ object smallCal extends Logging {
     // TODO Find the largest non negative average value
     val p = d.max
     p
-  } 
-
+  }
 }

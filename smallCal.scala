a5c158e149e595d813463776b9c552b0bea637c4
yiyinchin <cyy92@outlook.com>
2017-04-11 11:55:42 +1200

    Update smallCal.scala

--- smallCal.scala
+++ smallCal.scala
@@ -66,4 +66,66 @@ object smallCal extends Logging {
 
     diffSumArray.indexOf(maxDiffSum)
   }
+  
+   /**
+    * Returns the manhattan distance between two vectors.
+    *
+    * The manhattan distance is used to measure the diameter of a cluster with the largest diameter.
+    * @param v1 first Vector
+    * @param v2 Second Vector
+    * @return distance between two Vectors
+    */
+
+  def absVal(v1: Vector, v2: Vector): Double ={
+    require(v1.size == v2.size, s"Vector dimensions do not match: Dim(v1) = ${v1.size} and Dim(v2)" +
+      s"=${v2.size}.")
+    var distance = 0.0
+    (v1, v2) match {
+      case (v1: SparseVector, v2: SparseVector) =>
+        val v1Values = v1.values
+        val v1Indices = v1.indices
+        val v2Values = v2.values
+        val v2Indices = v2.indices
+        val nnzv1 = v1Indices.length
+        val nnzv2 = v2Indices.length
+
+        var kv1 = 0
+        var kv2 = 0
+        while (kv1 < nnzv1 || kv2 < nnzv2) {
+          var score = 0.0
+
+          if(kv2 >= nnzv2 || (kv1 < nnzv1 && v1Indices(kv1) < v2Indices(kv2))) {
+            score = v1Values(kv1)
+            kv1 += 1
+          } else if (kv1 >= nnzv1 || (kv2 < nnzv2 && v2Indices(kv2) < v1Indices(kv1))){
+            score = v2Values(kv2)
+            kv2 += 1
+          } else {
+            score = v1Values(kv1) - v2Values(kv2)
+            kv1 += 1
+            kv2 += 1
+          }
+          distance += math.abs(score)
+        }
+
+      case(v1: SparseVector, v2: DenseVector) =>
+        distance = absVal(v1, v2)
+
+      case(v1: DenseVector, v2: SparseVector) =>
+        distance = absVal(v2, v1)
+
+      case(DenseVector(vv1), DenseVector(vv2)) =>
+        var kv = 1
+        val sz = vv1.length
+        while(kv < sz) {
+          val score = vv1(kv) - vv2(kv)
+          distance += math.abs(score)
+          kv += 1
+        }
+      case _ =>
+        throw new IllegalArgumentException("Do not support vector type" + v1.getClass +
+          " and " + v2.getClass)
+    }
+    math.abs(distance)
+  }
 }
